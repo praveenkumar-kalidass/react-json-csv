75208770b15ba97268983c83dcf21285d68a7989
praveenkumar-outlook <praveen.ece@outlook.com>
2018-10-06 12:36:34 +0530

    Dev : App component

---
+++ dev/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  return (
+    <div>
+      App
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
