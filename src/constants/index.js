9be2b3de5d8aeaf107ea2413dbe3367da535f98d
Praveenkumar Kalidass <praveen.ece@outlook.com>
2020-08-24 20:29:08 +0530

    Code : Split string constants

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const CONSTANTS = {
+  DEFAULT_FILE_FORMAT: "csv",
+  DEFAULT_FILE_NAME: "json-to-excel",
+  CONVERT_LABEL: "Convert Json to Excel",
+  BLOB_TYPE_TEXT: "text/plain",
+  BLOB_CHARSET_UTF8: "utf-8",
+  DOT: ".",
+};
